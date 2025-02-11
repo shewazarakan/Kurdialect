46ceff9c79cafded4af70b62b4291a7863153a94
shewazarakan <shewazarakan@gmail.com>
2025-02-11 19:48:30 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('my-cache').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
-        '/manifest.json',
-        '/icon.png',
-        '/icon-512.png',
-        '/app.js',
-        '/styles.css'
+        '/styles.css',
+        '/App.js',
+        '/Icon.png',
+        '/Icon-512.png',
+        '/Manifest.json'
       ]);
     })
   );
@@ -18,21 +18,6 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['v1'];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
     })
   );
 });
