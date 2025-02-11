45caf343eab505290ba7983988e5214f193d8da4
shewazarakan <shewazarakan@gmail.com>
2025-02-11 21:16:23 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,27 @@
+const CACHE_NAME = "kurdish-translator-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/app.js",
+    "/manifest.json",
+    "/data.json",
+    "/icon.png",
+    "/icon-512.png"
+];
 
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
