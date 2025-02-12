5a1205c7e66469289874f20c66dc50c6c138c0f2
shewazarakan <shewazarakan@gmail.com>
2025-02-12 11:55:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,42 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('search-app-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/app.js',
-                '/icon-512.png'
-            ]);
-        })
-    );
+const CACHE_NAME = "kurdisiv-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "styles.css",
+  "app.js",
+  "icon-512.png",
+  "icon.png",
+  "manifest.json"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+// Fetch Resources
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    })
+  );
 });
