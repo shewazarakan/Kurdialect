0c9d7916d748bd182acd639de70f7080a50a0e72
shewazarakan <shewazarakan@gmail.com>
2025-02-12 14:22:32 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,23 @@
-const CACHE_NAME = "kurdisiv-cache-v1";
-const urlsToCache = [
-  "index.html",
-  "styles.css",
-  "app.js",
-  "icon-512.png",
-  "icon.png",
-  "manifest.json"
-];
-
-// Install Service Worker
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Fetch Resources
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Update Service Worker
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('kurdisiv-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                '/manifest.json',
+                '/icon-512.png',
+                '/icon.png'
+            ]);
         })
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
