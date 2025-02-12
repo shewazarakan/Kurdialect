28a9fb5342a7fba78a81e8162e9a3b4dfaabf8ce
shewazarakan <shewazarakan@gmail.com>
2025-02-12 14:41:54 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,12 @@
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open('kurdisiv-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/app.js',
-                '/manifest.json',
-                '/icon-512.png',
-                '/icon.png'
-            ]);
+        caches.open("kurdishiv-cache").then(cache => {
+            return cache.addAll(["index.html", "styles.css", "app.js", "manifest.json"]);
         })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
